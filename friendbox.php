b0dd4356a0485d2d8eb7aa4fbb514a0ca21379ea
Sean Stephens <seanastephens@email.arizona.edu>
2014-03-03 13:32:52 -0700

    Ran friendbox.php and scrape.php through a formatter.

--- friendbox.php
+++ friendbox.php
@@ -3,40 +3,40 @@ require 'facebook.php';
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-if(!isset($_GET["genre"])){
+if (!isset($_GET["genre"])) {
     die();
 }
 
-$genre = $_GET['genre'];
+$genre    = $_GET['genre'];
 $filename = 'genres/' . $genre . '.txt';
 
-if(file_exists($filename)) {
+if (file_exists($filename)) {
     $handle = fopen('genres/' . $genre . '.txt', "r");
     if ($handle) {
-        while(($line = fgets($handle)) !== false) {
-            $movietitle[] = trim($line);	
+        while (($line = fgets($handle)) !== false) {
+            $movietitle[] = trim($line);
         }
     } else {
-        echo "File opening failed."; 
+        echo "File opening failed.";
     }
 } else {
-    echo "File not found."; 
+    echo "File not found.";
     die();
 }
 
 $config = array(
-        'appId' => '299220656893010',
-        'secret' => '6d27ff1e8509d4fd017f16ed23cae89b',
-        'allowSignedRequest' => false, // optional, but should be false
-        'cookie' => true
+    'appId' => '299220656893010',
+    'secret' => '6d27ff1e8509d4fd017f16ed23cae89b',
+    'allowSignedRequest' => false, // optional, but should be false
+    'cookie' => true
 );
 
 $facebook = new Facebook($config);
-$user = $facebook->getUser();
+$user     = $facebook->getUser();
 
-if(!$user) {
+if (!$user) {
     $loginUrl = $facebook->getLoginUrl(array(
-            'scope' => 'user_friends, friends_likes, publish_stream', 
+        'scope' => 'user_friends, friends_likes, publish_stream'
     ));
     echo "Login using Facebook <a href=" . $loginUrl . ">here</a>";
     die();
@@ -48,23 +48,23 @@ $friends = $info['friends']['data'];
 
 echo '<form>';
 $foundFriend = false;
-for($i=0;$i<sizeof($friends);$i++){
-    for($j=0;$j<sizeof($movietitle);$j++){
-	    if(isset($friends[$i]['movies'])){
-		    for($k=0;$k<sizeof($friends[$i]['movies']['data']);$k++){
-			    if($friends[$i]['movies']['data'][$k]['name'] === $movietitle[$j] ) {
-				    $foundFriend = true;
-				    echo '<input type="checkbox" name="friend" value="' . trim($friends[$i]['name']) . '">';
-				    echo $friends[$i]['name'];
-				    echo '<br>';
-				    break 2;
-			    }
-		    }
-	    }
+for ($i = 0; $i < sizeof($friends); $i++) {
+    for ($j = 0; $j < sizeof($movietitle); $j++) {
+        if (isset($friends[$i]['movies'])) {
+            for ($k = 0; $k < sizeof($friends[$i]['movies']['data']); $k++) {
+                if ($friends[$i]['movies']['data'][$k]['name'] === $movietitle[$j]) {
+                    $foundFriend = true;
+                    echo '<input type="checkbox" name="friend" value="' . trim($friends[$i]['name']) . '">';
+                    echo $friends[$i]['name'];
+                    echo '<br>';
+                    break 2;
+                }
+            }
+        }
     }
 }
-echo '</form>';  
-if(!$foundFriend) {
-    echo 'No friends found :(';	
+echo '</form>';
+if (!$foundFriend) {
+    echo 'No friends found :(';
 }
-?>
+?> 
--- scrape.php
+++ scrape.php
@@ -1,65 +1,66 @@
 <?php
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-    $genre = $_GET['genre'];
-    $uid   = $_GET['uid'];
-    $locale   = "Tucson, Arizona";
-    $api_key = "kvscnr9agpmsxungb7ka4bza";
+$genre   = $_GET['genre'];
+$uid     = $_GET['uid'];
+$locale  = "Tucson, Arizona";
+$api_key = "kvscnr9agpmsxungb7ka4bza";
 
-    $let0 = " ";
-    $rep0 = "+";
-    $locale = str_replace($let0,$rep0,$locale);
-    $zip_url = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=1+main+st,+".$locale;
-    $zipj = file_get_contents($zip_url);
-    $z = json_decode($zipj,true);
-    $i = $z['results'][0]['address_components'];
-    $i = sizeof($i);
-    $zip = $z['results'][0]['address_components'][$i-1]['long_name'];
- //   echo $zip;
+$let0    = " ";
+$rep0    = "+";
+$locale  = str_replace($let0, $rep0, $locale);
+$zip_url = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=1+main+st,+" . $locale;
+$zipj    = file_get_contents($zip_url);
+$z       = json_decode($zipj, true);
+$i       = $z['results'][0]['address_components'];
+$i       = sizeof($i);
+$zip     = $z['results'][0]['address_components'][$i - 1]['long_name'];
+//   echo $zip;
 
-    $base_url = "http://data.tmsapi.com/v1/movies/showings?startDate=2014-03-01&zip=" . $zip . "&api_key=" . $api_key;
+$base_url = "http://data.tmsapi.com/v1/movies/showings?startDate=2014-03-01&zip=" . $zip . "&api_key=" . $api_key;
 //    $base_url = "http://data.tmsapi.com/v1/movies/showings?startDate=2014-03-01&zip=85364&api_key=" . $api_key;
 //    $base_url = "file.json";
-    
+
 //    echo $base_url; 
 
-    $page = file_get_contents($base_url);
-    $j = json_decode($page,true);
-    //var_dump($j,true);
-    for($i=0; $i < sizeof($j); $i++) {
+$page = file_get_contents($base_url);
+$j    = json_decode($page, true);
+//var_dump($j,true);
+for ($i = 0; $i < sizeof($j); $i++) {
     //--Get info
-        $tit   = $j[$i]['title'];
-        $year  = $j[$i]['releaseYear'];
-        $desc  = $j[$i]['shortDescription'];
-        $genres = $j[$i]['genres'];
-        $times = $j[$i]['showtimes'];
-        foreach($genres as $g) {
-//            echo $g . '&nbsp;&nbsp;' . $genre . '</br>';
-            if($g == $genre) {
+    $tit    = $j[$i]['title'];
+    $year   = $j[$i]['releaseYear'];
+    $desc   = $j[$i]['shortDescription'];
+    $genres = $j[$i]['genres'];
+    $times  = $j[$i]['showtimes'];
+    foreach ($genres as $g) {
+        //            echo $g . '&nbsp;&nbsp;' . $genre . '</br>';
+        if ($g == $genre) {
             //--Printing
-                echo '<br/>' . $tit . "  (" .  $year . ")";
-                echo '<br/>' . "[" . $genres[0];
-                for($k=1;$k<sizeof($genres);$k++) {
-                    echo"," .  $genres[$k];
-                }
-                echo "]";
-                echo '<br/>' . $desc;
-
-            //--Print Showtimes
-                for($k=0; $k<sizeof($times); $k++) {
-                    echo '<br/> <b>';
-                    echo $times[$k]['theatre']['name'] . '</b>';
-                    $let = "T";
-                    $rep = " ";
-                    $time = $times[$k]['dateTime'];
-                    $time = str_replace($let,$rep,$time);
-                    echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $time ;
-                    echo '&nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onClick="invite()">Invite</button>'; 
-                }
-                echo '<hr/>';
-
+            echo '<br/>' . $tit . "  (" . $year . ")";
+            echo '<br/>' . "[" . $genres[0];
+            for ($k = 1; $k < sizeof($genres); $k++) {
+                echo "," . $genres[$k];
             }
+            echo "]";
+            echo '<br/>' . $desc;
+            
+            //--Print Showtimes
+            for ($k = 0; $k < sizeof($times); $k++) {
+                echo '<br/> <b>';
+                echo $times[$k]['theatre']['name'] . '</b>';
+                $let  = "T";
+                $rep  = " ";
+                $time = $times[$k]['dateTime'];
+                $time = str_replace($let, $rep, $time);
+                echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $time;
+                echo '&nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onClick="invite()">Invite</button>';
+            }
+            echo '<hr/>';
+            
         }
     }
+}
 ?>
+Download
